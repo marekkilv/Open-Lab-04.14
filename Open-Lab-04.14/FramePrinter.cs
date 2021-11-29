1b2ebf8735b24bcee9fd3cf811bdc8a8155187e1
Marekmp@outlook.sk <Marekmp@outlook.sk>
2021-11-29 20:09:39 +0100

    mam

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -6,7 +6,26 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            string[] words = new[] { "Hello", "World", "in", "a", "frame" };
+        int length = 0;
+
+        foreach (var item in words)
+        {
+            if (length < item.Length)
+            {
+                length = item.Length;
+            }            
+        }
+        String tabs = new string('*', length + 4);
+
+        Console.WriteLine(tabs);
+        foreach (var item in words)
+        {
+            Console.WriteLine("* " + item.PadRight(length, ' ') + " *");
+        }
+        Console.WriteLine(tabs);
+        Console.ReadKey();
+
         }
     }
 }
